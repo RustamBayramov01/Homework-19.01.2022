2a0be54f63e885ede7d7f90d14db07b6e36bef23
RustamBayramov01 <rustam.bayramov.989@gmail.com>
2022-01-18 10:56:17 +0400

    Homework-19.01.2022

---
+++ ConsoleApp1/Capital.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    class Capital
+    {
+
+        public int PopulationX { get; set; }
+        public int PopulationY { get; set; }
+
+
+        public static bool operator <(Capital p1, Capital p2)
+        {
+            return p1.PopulationX < p2.PopulationY;
+
+        }
+
+        public static bool operator >(Capital p1, Capital p2)
+        {
+            return p1.PopulationX > p2.PopulationY;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{PopulationX}, {PopulationY}";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Capital1.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleApp1;
+using System;
+
+
+
+Moskova moskova = new() { PopulationX = 10000, PopulationY = 0 };
+Berlin berlin = new() { PopulationX = 200, PopulationY = 0 };
+
+
+Console.WriteLine(moskova > berlin);
+Console.WriteLine(moskova < berlin);
